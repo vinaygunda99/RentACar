112078092368ece5a4a7094fd4b2ff7e8bf980be
x22175270 <x22175270@student.ncirl.ie>
2024-12-03 13:03:56 +0530

    Namespace changes for pylint issue

--- RentACar/asgi.py
+++ RentACar/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for RentACar project.
+ASGI config for rentacar project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RentACar.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rentacar.settings')
 
 application = get_asgi_application()
