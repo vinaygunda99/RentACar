3066edcebbc4c5d28714243ae13eb5a5da2dbc65
x22175270 <x22175270@student.ncirl.ie>
2024-12-04 20:25:29 +0530

    changes

--- rentacar/asgi.py
+++ rentacar/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for rentacar project.
+ASGI config for RentACar project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rentacar.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RentACar.settings')
 
 application = get_asgi_application()
