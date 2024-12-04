23dbbe2386984ba1559dd01bda2a5a94bfc317a2
x22175270 <x22175270@student.ncirl.ie>
2024-12-05 00:08:12 +0530

    updates

--- rentacar/wsgi.py
+++ rentacar/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for RentACar project.
+WSGI config for rentacar project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RentACar.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rentacar.settings')
 
 application = get_wsgi_application()
 
